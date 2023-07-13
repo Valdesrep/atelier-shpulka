bf4a219c3da2d3091b887e62626508aa7181ee5b
Vladislav Boiko <90558354+Valdesrep@users.noreply.github.com>
2023-07-14 00:04:26 +0300

    fix size

--- js/contacts-modal.js
+++ js/contacts-modal.js
@@ -1,12 +1,15 @@
 (() => {
     const refs = {
       openModalBtn: document.querySelector('[data-open]'),
+      openModalBtnMob: document.querySelector('[data-open-mob]'),
       closeModalBtn: document.querySelector('[data-close]'),
       modal: document.querySelector('[data-modal-contacts]'),
     };
   
     refs.openModalBtn.addEventListener('click', toggleModal);
     refs.closeModalBtn.addEventListener('click', toggleModal);
+    refs.openModalBtnMob.addEventListener('click', toggleModal);
+
   
     function toggleModal() {
       refs.modal.classList.toggle('is-hidden');
